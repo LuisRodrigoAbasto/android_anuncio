b8dded53c51db06bcd8812113c58a4edf3fb6300
Luis Rodrigo Abasto <vida12345abasto@gmail.com>
2020-02-10 00:45:16 -0400

    abasto empezando android

---
+++ app/src/main/java/com/abasto/mianuncio/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.abasto.mianuncio.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
